5ae7cd75e0a4c58b8cb449c20089f71b195940b9
Finn O'Prey <finn@oprey.co>
2024-05-21 03:43:54 +1000

    Added basic error handling to main server component

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,5 @@
-import { onMount } from "svelte";
-import { page } from '$app/stores'
-import { redirect } from '@sveltejs/kit';
-import { DEV_ID, DEV_KEY, USE_FAKE, VERCEL_URL, VERCEL_ENV } from '$env/static/private';
+import { redirect, error } from '@sveltejs/kit'
+import { DEV_ID, DEV_KEY, USE_FAKE, VERCEL_URL, VERCEL_ENV } from '$env/static/private'
 
 const PTV_API_BASE_URL = 'https://timetableapi.ptv.vic.gov.au'
 const PTV_API_VERSION = 'v3'
@@ -10,6 +8,15 @@ const MAX_RESULTS = 2
 
 const BASE_URL = ( VERCEL_ENV == 'development' ? 'http://' : 'https://') + VERCEL_URL
 
+function handleError(error) {
+  let id = Math.floor(1000 + Math.random() * 9000)
+  console.error(`ERROR ${id}`)
+  console.error(error)
+  throw error(506, {
+    message: `Whoops. There was an unknown error. ID: ${id}`
+  })
+}
+
 export async function load({ url }) {
   const latitude = url.searchParams.get('latitude')
   const longitude = url.searchParams.get('longitude') 
@@ -17,7 +24,7 @@ export async function load({ url }) {
   console.log(VERCEL_URL)
 
   if (!latitude || !longitude) {
-    throw redirect(302, '/positioner');
+    throw redirect(302, '/positioner')
   }
 
   // type 0: train, type 1: tram, type 2: bus
@@ -25,38 +32,41 @@ export async function load({ url }) {
   async function ptv_request(content) {
     const combiner = content.includes('?') ? '&' : '?';
     const url_path = `/${PTV_API_VERSION}${content}${combiner}devid=${DEV_ID}`
-  
+    
     const signature_response = await fetch(`${BASE_URL}/api/hmacsha1?key=${DEV_KEY}&value=${encodeURIComponent(url_path)}`)
+      .catch(handleError)
     const signature = await signature_response.text()
   
-    const response = await fetch(`${PTV_API_BASE_URL}${url_path}&signature=${signature}`);
+    const response = await fetch(`${PTV_API_BASE_URL}${url_path}&signature=${signature}`)
+      .catch(handleError)
     const body = await response.json();
-
     return body;
   }
 
   if (USE_FAKE == "true") {
     let fake_data = await fetch(`${BASE_URL}/fake_data.json`)
+      .catch(handleError)
     return fake_data.json()
   } else {
     let data = await ptv_request(`/stops/location/${latitude},${longitude}?max_distance=${DISTANCE}&max_results=${MAX_RESULTS}`)
-    
+
     for (let stop of data.stops) {
       for (let route of stop.routes) {
         let departure_data = await ptv_request(`/departures/route_type/${route.route_type}/stop/${stop.stop_id}/route/${route.route_id}?max_results=2&expand=Direction`)
+          .catch(handleError)
         route.directions = departure_data.directions
         
         for (let departure of departure_data.departures) {
-          let direction_to_id = departure.direction_id;
+          let direction_to_id = departure.direction_id
       
-          let route_directions_list = Object.values(route.directions).map(direction => direction.direction_id);
+          let route_directions_list = Object.values(route.directions).map(direction => direction.direction_id)
       
           // make list of direction id numbers
-          let direction_from_id = route_directions_list.find(number => number !== direction_to_id);
+          let direction_from_id = route_directions_list.find(number => number !== direction_to_id)
       
           // find direction names using direction id's
-          let direction_to_name = route.directions[direction_to_id]?.direction_name || '';
-          let direction_from_name = route.directions[direction_from_id]?.direction_name || '';
+          let direction_to_name = route.directions[direction_to_id]?.direction_name || ''
+          let direction_from_name = route.directions[direction_from_id]?.direction_name || ''
       
           // set direction names to departure object
           departure.direction_from_name = direction_from_name;
